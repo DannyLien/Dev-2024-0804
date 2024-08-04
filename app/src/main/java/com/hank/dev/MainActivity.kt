a37a4eb922b0c914f9c92ec25a1d51e8cdabc494
DannyLien <danny.lien1109@gmail.com>
2024-08-04 10:34:42 +0800

    10-3, initial, Dev Project, json, URL;

---
+++ app/src/main/java/com/hank/dev/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.hank.dev
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.hank.dev.databinding.ActivityMainBinding
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG: String? = MainActivity::class.java.simpleName
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        Thread {
+            val json = URL("https://api.jsonserve.com/pcLzBT").readText()
+            Log.d(TAG, "onCreate: ${json}")
+        }.start()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+}
